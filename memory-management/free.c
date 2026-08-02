c8ce9b4c1ac62d6a1d33ac91b5472831fb78b7ad
Guru Prassad Mishra <2020gurup@gmail.com>
2026-08-02 15:34:41 +0530

    Update free.c

---
+++ memory-management/free.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n;
+    int *arr;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("\nEnter %d integers:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nArray Elements:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    free(arr);
+
+    printf("\nMemory has been freed successfully.\n");
+
+    arr = NULL;
+
+    if (arr == NULL)
+    {
+        printf("Pointer is now NULL.\n");
+    }
+
+    return 0;
+}
